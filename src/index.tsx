34eccd4ffd76fe2ad73ed217d7aaf36af98649c4
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-10-05 00:02:54 -0400

    add mock server in dev mode

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const { worker } = require('./mocks/browser');
-worker.start();
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
 
 ReactDOM.render(
   <React.StrictMode>
